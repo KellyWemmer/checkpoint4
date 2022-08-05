8fcf2f367bcde1f34a507268a9a4d7fb9c58db7a
Kelly <clear>
2022-08-05 16:33:52 -0600

    image to background

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -9,7 +9,8 @@ function _drawImage() {
 
 export class ImageController {
     constructor () {
-        this.getImageFromApi() 
+        this.getImageFromApi()
+        document.body.style.backgroundImage = "url('https://pixabay.com/get/gfd7b6427f5a592daff91c5b4d04d909174598194585e5c3abc8c200e60bdbfb3f38bdae5c0d0c12e31e81b8bbac9a55009f076b6edeb999491a0febdc7b0785d_1280.jpg')"
         ProxyState.on('image', _drawImage)   
     }
     async getImageFromApi() {
@@ -17,9 +18,7 @@ export class ImageController {
             await imageService.getImage()
         } catch (error) {
             console.error('[getting Image in controller]', error) 
-            Pop.error(error) 
-
-            
+            Pop.error(error)             
         }
         
     }        
