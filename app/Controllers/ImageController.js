4631a10004ddafdbe204b60fb420470fb13e6b9e
Kelly <clear>
2022-08-05 15:19:30 -0600

    fix bugs

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -2,18 +2,26 @@ import { ProxyState } from "../AppState.js";
 import { imageService } from "../Services/ImageService.js";
 import {Pop} from "../Utils/Pop.js";
 
+function _drawImage() {
+    document.getElementById('background-img').innerHTML = ProxyState.image.BgImageTemplate        
+}
+
 export class ImageController {
     constructor () {
-        console.log("testing controller")//Successful log
-
+        this.getImageFromApi() 
+        ProxyState.on('image', _drawImage)   
+    }
     async getImageFromApi() {
         try {
             await imageService.getImage()
         } catch (error) {
             console.error('[getting Image in controller]', error) 
             Pop.error(error) 
+
+            
         }
+        
     }        
         
     }   
-}
+
--- app/Services/ImageService.js
+++ app/Services/ImageService.js
@@ -1,11 +1,14 @@
 import { ProxyState } from "../AppState.js";
 import {BgImage} from "../Models/Image.js";
 import { ImageController } from "../Controllers/ImageController.js";
+import { api } from "./AxiosService.js";
 
 class ImageService {
     async getImage() {
-        let res = await axios.get('/images')
+        let res = await api.get('/images')
         console.log('get image response service', res)
+
+        ProxyState.image = res.data.results
     }
 }
 
