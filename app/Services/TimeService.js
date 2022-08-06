b46a0b12312f9f17ed0ed04079bb22cd35d88564
Kelly <clear>
2022-08-06 17:03:02 -0600

    Printed Time to page

---
+++ app/Services/TimeService.js
@@ -0,0 +1,15 @@
+import { ProxyState } from "../AppState.js"
+import { Time } from "../Models/Time.js";
+import { Todo} from "../Models/Todo.js"
+
+class TimeService {
+    getTime() {
+        let today = new Date();
+
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+       
+        ProxyState.time = new Time(time)
+    }
+}
+
+export const timeService = new TimeService()
